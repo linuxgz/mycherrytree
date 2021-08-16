9a5d7b7197efb3e586eb9606f720969e287fca97
linuxgz <linuxgz@163.com>
2021-08-16 18:25:40 +0800

    save them

--- git_mychreeytree.sh
+++ git_mychreeytree.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # update git mysh
-cd ~/mychreeytree
-git add .* * && git commit -a -m "save them" 
+git add * && git commit -a -m "save them" 
 git push git@github.com:linuxgz/mychreeytree main
